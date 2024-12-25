f93d03e1469d3af9adfb65fac7b6c3a556b67d50
Abdullah <abdullah12.hassan7@gmail.com>
2024-12-25 21:44:32 +0200

    not used func removed

--- resnet.py
+++ resnet.py
@@ -159,17 +159,6 @@ class FundRandomRotate:
         return img
 
 
-# ADDED FUNCTION TO ADD GAUSSIAN NOISE...
-
-# transform_train = transforms.Compose([
-#     transforms.Resize((256, 256)),
-#     transforms.RandomCrop((224, 224)),  # Slightly larger crop to retain features.
-#     transforms.RandomHorizontalFlip(p=0.5),
-#     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
-#     FundRandomRotate(prob=0.3, degree=20),  # Reduce probability and degree.
-#     transforms.ToTensor(),
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
 
 transform_train = transforms.Compose([
     transforms.Resize((256, 256)),
