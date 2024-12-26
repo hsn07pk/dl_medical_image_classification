b4cc7480fe58d8124362e76f636d860cfab2bf46
Abdullah <abdullah12.hassan7@gmail.com>
2024-12-26 03:55:12 +0200

    resnet50 working with new dataset

--- resnet50partB.py
+++ resnet50partB.py
@@ -1,26 +1,27 @@
-import numpy as np
-import pandas as pd
+import copy
 import os
 import random
-import copy
 import sys
-from PIL import Image
 
+import numpy as np 
+import pandas as pd
 import torch
 import torch.nn as nn
+from PIL import Image
 from sklearn.metrics import cohen_kappa_score, precision_score, recall_score, accuracy_score
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from torchvision import models, transforms
+from torchvision.transforms.functional import to_pil_image
 from tqdm import tqdm
 
-# Hyperparameters
+# Hyper Parameters
 batch_size = 24
-num_classes = 5
+num_classes = 5  # 5 DR levels
 learning_rate = 0.0001
-num_epochs = 20
+num_epochs = 10
 
-# Dataset Definition
+
 class DiabeticRetinopathyDataset(Dataset):
     def __init__(self, df, image_dir, transform=None):
         self.df = df
@@ -41,7 +42,40 @@ class DiabeticRetinopathyDataset(Dataset):
         label = torch.tensor(level, dtype=torch.long)
         return image, label
 
-# Data Preparation
+class RetinopathyDataset(Dataset):
+    def __init__(self, dataframe, image_dir, transform=None, mode='single', test=False):
+        self.dataframe = dataframe
+        self.image_dir = image_dir
+        self.transform = transform
+        self.mode = mode
+        self.test = test
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        if self.mode == 'single':
+            return self.get_item(index)
+        else:
+            return self.get_item_dual(index)
+
+    def get_item(self, index):
+        row = self.dataframe.iloc[index]
+        img_path = os.path.join(self.image_dir, f"{row['image']}.jpeg")
+        img = Image.open(img_path).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+
+        if not self.test:
+            label = torch.tensor(row['level'], dtype=torch.int64)
+            return img, label
+        else:
+            return img
+
+    def get_item_dual(self, index):
+        raise NotImplementedError("Dual mode is not supported in this dataset.")
+
+
 def load_and_balance_data(csv_path, image_dir):
     df = pd.read_csv(csv_path)
 
@@ -55,16 +89,68 @@ def load_and_balance_data(csv_path, image_dir):
     balanced_df = df.groupby('level').apply(lambda x: x.sample(min_samples, random_state=42)).reset_index(drop=True)
 
     # Split the balanced dataset for training
-    train_df, _ = train_test_split(balanced_df, test_size=0.2, stratify=balanced_df['level'], random_state=42)
-    return train_df, df
+    train_df = balanced_df
+    return train_df
 
-# Transformations
+class CutOut(object):
+    def __init__(self, mask_size, p=0.5):
+        self.mask_size = mask_size
+        self.p = p
+
+    def __call__(self, img):
+        if np.random.rand() > self.p:
+            return img
+
+        if not isinstance(img, torch.Tensor):
+            raise TypeError('Input image must be a torch.Tensor')
+
+        h, w = img.shape[1], img.shape[2]
+        mask_size_half = self.mask_size // 2
+        offset = 1 if self.mask_size % 2 == 0 else 0
+
+        cx = np.random.randint(mask_size_half, w + offset - mask_size_half)
+        cy = np.random.randint(mask_size_half, h + offset - mask_size_half)
+
+        xmin, xmax = cx - mask_size_half, cx + mask_size_half + offset
+        ymin, ymax = cy - mask_size_half, cy + mask_size_half + offset
+        xmin, xmax = max(0, xmin), min(w, xmax)
+        ymin, ymax = max(0, ymin), min(h, ymax)
+
+        img[:, ymin:ymax, xmin:xmax] = 0
+        return img
+
+class SLORandomPad:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, img):
+        pad_width = max(0, self.size[0] - img.width)
+        pad_height = max(0, self.size[1] - img.height)
+        pad_left = random.randint(0, pad_width)
+        pad_top = random.randint(0, pad_height)
+        pad_right = pad_width - pad_left
+        pad_bottom = pad_height - pad_top
+        return transforms.functional.pad(img, (pad_left, pad_top, pad_right, pad_bottom))
+
+class FundRandomRotate:
+    def __init__(self, prob, degree):
+        self.prob = prob
+        self.degree = degree
+
+    def __call__(self, img):
+        if random.random() < self.prob:
+            angle = random.uniform(-self.degree, self.degree)
+            return transforms.functional.rotate(img, angle)
+        return img
+
 transform_train = transforms.Compose([
     transforms.Resize((256, 256)),
-    transforms.RandomCrop((224, 224)),
+    transforms.RandomCrop((210, 210)),
+    SLORandomPad((224, 224)),
+    FundRandomRotate(prob=0.5, degree=30),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomVerticalFlip(p=0.5),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.ColorJitter(brightness=(0.1, 0.9)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
@@ -75,110 +161,239 @@ transform_test = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Model Definition
-class DRModel(nn.Module):
-    def __init__(self, num_classes=5, dropout_rate=0.5):
-        super(DRModel, self).__init__()
-        self.backbone = models.resnet50(pretrained=False)
-        self.backbone.fc = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.ReLU(),
+def train_model(model, train_loader, val_loader, device, criterion, optimizer, lr_scheduler, num_epochs=25,
+                checkpoint_path='model.pth'):
+    best_model = model.state_dict()
+    best_epoch = None
+    best_val_kappa = -1.0
+
+    for epoch in range(1, num_epochs + 1):
+        print(f'\nEpoch {epoch}/{num_epochs}')
+        running_loss = []
+        all_preds = []
+        all_labels = []
+
+        model.train()
+
+        with tqdm(total=len(train_loader), desc=f'Training', unit=' batch', file=sys.stdout) as pbar:
+            for images, labels in train_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                outputs = model(images)
+                loss = criterion(outputs, labels.long())
+
+                loss.backward()
+                optimizer.step()
+
+                preds = torch.argmax(outputs, 1)
+                all_preds.extend(preds.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+
+                running_loss.append(loss.item())
+
+                pbar.set_postfix({'lr': f'{optimizer.param_groups[0]["lr"]:.1e}', 'Loss': f'{loss.item():.4f}'})
+                pbar.update(1)
+
+        lr_scheduler.step()
+
+        epoch_loss = sum(running_loss) / len(running_loss)
+
+        train_metrics = compute_metrics(all_preds, all_labels, per_class=True)
+        kappa, accuracy, precision, recall = train_metrics[:4]
+
+        print(f'[Train] Kappa: {kappa:.4f} Accuracy: {accuracy:.4f} '
+              f'Precision: {precision:.4f} Recall: {recall:.4f} Loss: {epoch_loss:.4f}')
+
+        if len(train_metrics) > 4:
+            precision_per_class, recall_per_class = train_metrics[4:]
+            for i, (precision, recall) in enumerate(zip(precision_per_class, recall_per_class)):
+                print(f'[Train] Class {i}: Precision: {precision:.4f}, Recall: {recall:.4f}')
+
+        val_metrics = evaluate_model(model, val_loader, device)
+        val_kappa, val_accuracy, val_precision, val_recall = val_metrics[:4]
+        print(f'[Val] Kappa: {val_kappa:.4f} Accuracy: {val_accuracy:.4f} '
+              f'Precision: {val_precision:.4f} Recall: {val_recall:.4f}')
+
+        if val_kappa > best_val_kappa:
+            best_val_kappa = val_kappa
+            best_epoch = epoch
+            best_model = model.state_dict()
+            torch.save(best_model, checkpoint_path)
+
+    print(f'[Val] Best kappa: {best_val_kappa:.4f}, Epoch {best_epoch}')
+
+    return model
+
+def evaluate_model(model, test_loader, device, test_only=False, prediction_path='./test_predictions.csv'):
+    model.eval()
+
+    all_preds = []
+    all_labels = []
+    all_image_ids = []
+
+    with tqdm(total=len(test_loader), desc=f'Evaluating', unit=' batch', file=sys.stdout) as pbar:
+        for i, data in enumerate(test_loader):
+
+            if test_only:
+                images = data
+            else:
+                images, labels = data
+
+            images = images.to(device)
+
+            with torch.no_grad():
+                outputs = model(images)
+                preds = torch.argmax(outputs, 1)
+
+            all_preds.extend(preds.cpu().numpy())
+            if not test_only:
+                all_labels.extend(labels.numpy())
+
+            pbar.update(1)
+
+    if test_only:
+        df = pd.DataFrame({
+            'ID': [os.path.basename(row['image']) for _, row in test_loader.dataset.dataframe.iterrows()],
+            'TARGET': all_preds
+        })
+        df.to_csv(prediction_path, index=False)
+        print(f'[Test] Save predictions to {os.path.abspath(prediction_path)}')
+    else:
+        metrics = compute_metrics(all_preds, all_labels)
+        return metrics
+
+def compute_metrics(preds, labels, per_class=False):
+    kappa = cohen_kappa_score(labels, preds, weights='quadratic')
+    accuracy = accuracy_score(labels, preds)
+    precision = precision_score(labels, preds, average='weighted', zero_division=0)
+    recall = recall_score(labels, preds, average='weighted', zero_division=0)
+
+    # Calculate and print precision and recall for each class
+    if per_class:
+        precision_per_class = precision_score(labels, preds, average=None, zero_division=0)
+        recall_per_class = recall_score(labels, preds, average=None, zero_division=0)
+        return kappa, accuracy, precision, recall, precision_per_class, recall_per_class
+
+    return kappa, accuracy, precision, recall
+
+class MyModel(nn.Module):
+    def __init__(self, num_classes=5, dropout_rate=0.52):
+        super().__init__()
+
+        self.backbone = models.resnet18(pretrained=True)
+        self.backbone.fc = nn.Identity()  # Remove the original classification layer
+
+        self.fc = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
             nn.Dropout(p=dropout_rate),
-            nn.Linear(512, num_classes)
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
-        return self.backbone(x)
+        x = self.backbone(x)
+        x = self.fc(x)
+        return x
 
-# Training and Evaluation Functions
-def train_model(model, train_loader, val_loader, device, criterion, optimizer, scheduler, num_epochs):
-    best_model_weights = copy.deepcopy(model.state_dict())
-    best_kappa = -1
 
-    for epoch in range(num_epochs):
-        print(f"Epoch {epoch + 1}/{num_epochs}")
+class MyDualModel(nn.Module):
+    def __init__(self, num_classes=5, dropout_rate=0.5):
+        super().__init__()
 
-        # Training phase
-        model.train()
-        running_loss = 0.0
-        train_preds, train_labels = [], []
+        backbone = models.resnet18(pretrained=True)
+        backbone.fc = nn.Identity()
 
-        for images, labels in tqdm(train_loader, desc="Training"):
-            images, labels = images.to(device), labels.to(device)
+        # Here the two backbones will have the same structure but unshared weights
+        self.backbone1 = copy.deepcopy(backbone)
+        self.backbone2 = copy.deepcopy(backbone)
 
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 2, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(128, num_classes)
+        )
 
-            loss.backward()
-            optimizer.step()
+    def forward(self, images):
+        image1, image2 = images
 
-            running_loss += loss.item() * images.size(0)
-            train_preds.extend(torch.argmax(outputs, 1).cpu().numpy())
-            train_labels.extend(labels.cpu().numpy())
+        x1 = self.backbone1(image1)
+        x2 = self.backbone2(image2)
 
-        scheduler.step()
+        x = torch.cat((x1, x2), dim=1)
+        x = self.fc(x)
+        return x
 
-        epoch_loss = running_loss / len(train_loader.dataset)
-        train_kappa = cohen_kappa_score(train_labels, train_preds, weights='quadratic')
-        print(f"Train Loss: {epoch_loss:.4f} | Train Kappa: {train_kappa:.4f}")
 
-        # Validation phase
-        model.eval()
-        val_preds, val_labels = [], []
-        val_loss = 0.0
 
-        with torch.no_grad():
-            for images, labels in tqdm(val_loader, desc="Validation"):
-                images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
-                loss = criterion(outputs, labels)
+if __name__ == '__main__':
+    # Choose between 'single image' and 'dual images' pipeline
+    # This will affect the model definition, dataset pipeline, training and evaluation
 
-                val_loss += loss.item() * images.size(0)
-                val_preds.extend(torch.argmax(outputs, 1).cpu().numpy())
-                val_labels.extend(labels.cpu().numpy())
+    mode = 'single'  # forward single image to the model each time
+    # mode = 'dual'  # forward two images of the same eye to the model and fuse the features
 
-        epoch_val_loss = val_loss / len(val_loader.dataset)
-        val_kappa = cohen_kappa_score(val_labels, val_preds, weights='quadratic')
-        print(f"Val Loss: {epoch_val_loss:.4f} | Val Kappa: {val_kappa:.4f}")
+    assert mode in ('single', 'dual')
 
-        if val_kappa > best_kappa:
-            best_kappa = val_kappa
-            best_model_weights = copy.deepcopy(model.state_dict())
+    # Define the model
+    if mode == 'single':
+        model = MyModel()
+    else:
+        model = MyDualModel()
 
-    model.load_state_dict(best_model_weights)
-    return model
+    print(model, '\n')
+    print('Pipeline Mode:', mode)
+    
+    train_csv_path = "./Bdataset/trainLabels.csv"
+    train_image_dir = "./Bdataset/resized_train_cropped/resized_train_cropped"
 
-# Main Pipeline
-def main():
-    csv_path = "./7/trainLabels.csv"
-    image_dir = "./7/resized_train_cropped/resized_train_cropped"
+    # Create datasets
+    train_df = load_and_balance_data(train_csv_path, train_image_dir)
+    
+    
+    train_dataset = DiabeticRetinopathyDataset(train_df, train_image_dir, transform=transform_train)
+    # train_dataset = RetinopathyDataset('./DeepDRiD/train.csv', './DeepDRiD/train/', transform_train, mode)
+    val_dataset = RetinopathyDataset('./DeepDRiD/val.csv', './DeepDRiD/val/', transform_test, mode)
+    test_dataset = RetinopathyDataset('./DeepDRiD/test.csv', './DeepDRiD/test/', transform_test, mode, test=True)
 
-    # Load and balance the data
-    train_df, val_df = load_and_balance_data(csv_path, image_dir)
-
-    # Create datasets and dataloaders
-    train_dataset = DiabeticRetinopathyDataset(train_df, image_dir, transform=transform_train)
-    val_dataset = DiabeticRetinopathyDataset(val_df, image_dir, transform=transform_test)
-
+    # Create dataloaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    # Initialize the model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Check for GPU
-    model = DRModel(num_classes=num_classes).to(device)  # Move model to device
-
-    # Define loss function, optimizer, and scheduler
+    # Define the weighted CrossEntropyLoss
     criterion = nn.CrossEntropyLoss()
+
+    # Use GPU device is possible
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Device:', device)
+
+    # Move class weights to the device
+    model = model.to(device)
+
+    # Optimizer and Learning rate scheduler
     optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
-    # Train and validate the model
-    model = train_model(model, train_loader, val_loader, device, criterion, optimizer, scheduler, num_epochs)
+    # Train and evaluate the model with the training and validation set
+    model = train_model(
+        model, train_loader, val_loader, device, criterion, optimizer,
+        lr_scheduler=lr_scheduler, num_epochs=num_epochs,
+        checkpoint_path='./model_1.pth'
+    )
 
-    # Save the trained model
-    torch.save(model.state_dict(), "dr_model.pth")
-    print("Model training complete and saved.")
+    # Load the pretrained checkpoint
+    state_dict = torch.load('./model_1.pth', map_location='cpu')
+    model.load_state_dict(state_dict, strict=True)
 
-if __name__ == "__main__":
-    main()
+    # Make predictions on testing set and save the prediction results
+    evaluate_model(model, test_loader, device, test_only=True)
