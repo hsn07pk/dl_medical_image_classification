87c9b237e1d21695c46bbf309428d0665ed87583
Abdullah <abdullah12.hassan7@gmail.com>
2024-12-26 02:27:12 +0200

    moved part a to new folder

--- downloadBDataSet.py
+++ downloadBDataSet.py
@@ -2,12 +2,12 @@ import os
 import kagglehub
 
 # Define the dataset path (or the directory where you want to store the dataset)
-dataset_path = "./Bdataset"
+dataset_path = "./Bdataset2"
 
 # Check if the dataset already exists
 if not os.path.exists(dataset_path):
     # Download the dataset
-    path = kagglehub.dataset_download("tanlikesmath/diabetic-retinopathy-resized")
+    path = kagglehub.dataset_download("mariaherrerot/aptos2019")
     print("Dataset downloaded to:", path)
 else:
     print("Dataset already exists at:", dataset_path)
